4da1a36335bb06b94c9b24aa1805cc200edd515e
-Lunat1c- <arturgrabowski100@onet.pl>
2023-04-08 13:44:38 +0200

    Created console GraphLib showcase app

---
+++ GraphConole/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+using GraphLib;
+
+namespace GraphConsole
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Graph graph = new Graph();
+            Console.WriteLine("Hello graph");            
+        }
+    }
+}
